ecf266db4b2a178d4431d10cc23980c9be4069c6
Ben Whittle <benwhittle31@gmail.com>
2017-11-29 20:50:23 -0800

    adding defintions.py and figured out netrc comes for free :)

--- tensorci/utils/api.py
+++ tensorci/utils/api.py
@@ -1,7 +1,8 @@
 import os
 from abstract_api import AbstractApi, ApiException
 from tensorci import config
+from tensorci.definitions import auth_header_name
 
 api = AbstractApi(base_url=config.API_URL,
-                  auth_header_name='TensorCI-Api-Token',
+                  auth_header_name=auth_header_name,
                   auth_header_value=get_token)
--- tensorci/utils/auth.py
+++ tensorci/utils/auth.py
@@ -1,2 +1,6 @@
+import netrc
+from tensorci.definitions import auth_header_name
+
+
 def get_token():
   return 'asdfasdasdasf'
