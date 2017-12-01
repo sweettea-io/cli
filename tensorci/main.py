5b3fb641a036b176066f8f5abfcc9889a88b7b3f
Ben Whittle <benwhittle31@gmail.com>
2017-11-30 18:41:57 -0800

    adding -h as help alias

--- tensorci/main.py
+++ tensorci/main.py
@@ -1,8 +1,10 @@
 import click
 from commands import all_cmds
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.group()
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
   pass
 
