2d18987f75c6cba5d9069751fefd3af5b754007f
Ben Whittle <benwhittle31@gmail.com>
2018-02-22 14:34:46 -0500

    ensuring clean model replacements

--- tensorci/helpers/file_helper.py
+++ tensorci/helpers/file_helper.py
@@ -2,6 +2,7 @@
 Helper methods related to files or their respective paths
 """
 import os
+import shutil
 import zipfile
 from tensorci import log
 
@@ -66,7 +67,7 @@ def upsert_parent_dirs(filepath):
 
 def extract_in_place(archive_path):
   """
-  Unpack a zipfile in place.
+  Unpack a zipfile in place and remove it upon completion.
 
   Ex:
     extract_in_place('path/to/archive.zip')
@@ -80,9 +81,16 @@ def extract_in_place(archive_path):
   filename_w_ext = archive_path.split('/').pop()
   extract_dir = archive_path[:-(len(filename_w_ext))]
 
+  # Remove the destination dir if it exists
+  if os.path.exists(extract_dir) and os.path.isdir(extract_dir):
+    shutil.rmtree(extract_dir)
+
   # Unpack the archive
   archive = zipfile.ZipFile(archive_path)
   archive.extractall(extract_dir)
   archive.close()
+
+  # Remove the archive file
+  os.remove(archive_path)
 
   return extract_dir
