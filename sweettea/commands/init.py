3922cdc47670f06e9344200d8a4f8f18c1f24794
Ben Whittle <benwhittle31@gmail.com>
2018-09-02 15:16:45 -0700

    fix bad function path formats in config defaults

--- sweettea/commands/init.py
+++ sweettea/commands/init.py
@@ -22,34 +22,34 @@ def init():
 
   Ex: $ st init
   """
-  # Must already be logged in to perform this command.
-  auth_required()
-
-  # If the current directory already has a config file, tell the user and exit.
-  if os.path.exists(config_file_path()):
-    log('SweetTea project already exists for this directory.')
-    return
-
-  # Find this git project's remote url namespace from inside .git/config
-  git_repo_nsp = gitconfig.get_remote_nsp()
-
-  try:
-    # Register the git repository as a SweetTea project.
-    api.post('/project', payload={'nsp': git_repo_nsp})
-  except KeyboardInterrupt:
-    return
+  # # Must already be logged in to perform this command.
+  # auth_required()
+  #
+  # # If the current directory already has a config file, tell the user and exit.
+  # if os.path.exists(config_file_path()):
+  #   log('SweetTea project already exists for this directory.')
+  #   return
+  #
+  # # Find this git project's remote url namespace from inside .git/config
+  # git_repo_nsp = gitconfig.get_remote_nsp()
+  # 
+  # try:
+  #   # Register the git repository as a SweetTea project.
+  #   api.post('/project', payload={'nsp': git_repo_nsp})
+  # except KeyboardInterrupt:
+  #   return
 
   # Unmarshal placeholder values into the SweetTea config file for the user to start with.
   config.unmarshal({
     'training': {
       'buildpack': train_buildpacks[0],
       'dataset': {
-        'fetch': 'mod1.mod2.fetch_dataset_func_name',
-        'prepro': 'mod1.mod2.preprocess_dataset_func_name'
+        'fetch': 'mod1.mod2:fetch_dataset_func_name',
+        'prepro': 'mod1.mod2:preprocess_dataset_func_name'
       },
-      'train': 'mod1.mod2.train_func_name',
-      'test': 'mod1.mod2.test_func_name',
-      'eval': 'mod1.mod2.eval_func_name',
+      'train': 'mod1.mod2:train_func_name',
+      'test': 'mod1.mod2:test_func_name',
+      'eval': 'mod1.mod2:eval_func_name',
       'model': {
         'path': 'rel/path/to/model/dest',
         'upload_criteria': 'always'
@@ -57,7 +57,7 @@ def init():
     },
     'hosting': {
       'buildpack': api_buildpacks[0],
-      'predict': 'mod1.mod2.predict_func_name',
+      'predict': 'mod1.mod2:predict_func_name',
       'model': {
         'path': 'rel/path/to/model/source',
       }
