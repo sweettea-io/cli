f64106fb26a950b6c88692fcf524cef8585c60d6
Ben Whittle <benwhittle31@gmail.com>
2018-09-02 15:35:50 -0700

    minor styling

--- sweettea/utils/api.py
+++ sweettea/utils/api.py
@@ -6,7 +6,7 @@ from sweettea.definitions import auth_header_name
 from sweettea.utils.abstract_api import AbstractApi
 from sweettea.utils.auth import get_password
 
-# Configure a SweetTea API client
+# Configure SweetTea API client.
 api = AbstractApi(base_url=config.API_URL,
                   auth_header_name=auth_header_name,
                   auth_header_val_getter=get_password)
