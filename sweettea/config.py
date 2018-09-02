8d7a209993e78f011b597b8cc6e570083c398b1c
Ben Whittle <benwhittle31@gmail.com>
2018-09-01 18:11:00 -0700

    add API_VERSION as config

--- sweettea/config.py
+++ sweettea/config.py
@@ -8,6 +8,7 @@ class Config:
   def __init__(self):
     self.DEBUG = os.environ.get('DEBUG', True)
     self.DOMAIN = os.environ.get('DOMAIN', 'api.sweettea.io')
-    self.API_URL = os.environ.get('API_URL', 'https://{}'.format(self.DOMAIN))
+    self.API_VERSION = os.environ.get('API_VERSION', 'v1')
+    self.API_URL = os.environ.get('API_URL', 'https://{}/{}'.format(self.DOMAIN, self.API_VERSION))
 
 config = Config()
