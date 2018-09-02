805f5caeb9b6a9557e7e63612d4d5e439b4068a7
Ben Whittle <benwhittle31@gmail.com>
2018-09-01 23:40:18 -0700

    serve --> host

--- sweettea/commands/__init__.py
+++ sweettea/commands/__init__.py
@@ -3,7 +3,7 @@ from sweettea.commands.init import init
 from sweettea.commands.login import login
 from sweettea.commands.logout import logout
 from sweettea.commands.logs import logs
-from sweettea.commands.serve import serve
+from sweettea.commands.host import host
 from sweettea.commands.train import train
 from sweettea.commands.version import version
 
@@ -13,7 +13,7 @@ all_cmds = [
   login,
   logout,
   logs,
-  serve,
+  host,
   train,
   version
 ]
