2cb38ac3df8b8cf4c6ca3affb3f2dafe9c9fd29c
Ben Whittle <benwhittle31@gmail.com>
2018-09-16 15:19:16 -0700

    add name and path args to upload model command

--- sweettea/resources/model.py
+++ sweettea/resources/model.py
@@ -1,10 +1,13 @@
 import click
+from sweettea.definitions import default_model_name
 
 MODEL_CMD = 'model'
 
 
 @click.command(name=MODEL_CMD)
-def upload():
+@click.option('--name', '-n', default=default_model_name)
+@click.option('--path', required=True)
+def upload(name, path):
   pass
 
 
