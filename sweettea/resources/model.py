da47e7aef162fbc9797e67f6b267280748ab5d8c
Ben Whittle <benwhittle31@gmail.com>
2018-09-17 17:43:51 -0700

    refactor model upload cmd logic into consolidated file_uploader logic

--- sweettea/resources/model.py
+++ sweettea/resources/model.py
@@ -1,12 +1,9 @@
 import click
-import mimetypes
-from requests_toolbelt.multipart.encoder import MultipartEncoder, MultipartEncoderMonitor
 from sweettea import log
-from sweettea.definitions import default_model_name, default_mime_type
-from sweettea.utils import file_util, model_util
+from sweettea.definitions import default_model_name
 from sweettea.utils.api import api
-from sweettea.utils.multipart_request_util import create_callback
-from sweettea.utils.payload_util import project_payload
+from sweettea.utils.file_util import new_file_uploader
+from sweettea.utils.model_util import upload_ready_model_path
 
 MODEL_CMD = 'model'
 
@@ -15,48 +12,17 @@ MODEL_CMD = 'model'
 @click.option('--name', '-n', default=default_model_name)
 @click.option('--path', required=True)
 def upload(name, path):
-  # Get exact model path to upload from provided path (if path is directory, it is compressed first).
-  model_path = model_util.upload_ready_model_path(path)
+  # Get exact model path to upload from provided path (if directory, it's compressed first).
+  model_path = upload_ready_model_path(path)
 
-  # Name of file used in upload attachment.
-  upload_file_key = 'model'
-
-  # Get the file extension for the model file.
-  model_ext = file_util.file_ext(model_path)
-
-  # Add model extension to upload file key (if exists).
-  if model_ext:
-    upload_file_key = '.'.join((upload_file_key, model_ext))
-
-  # Get model's mime type based on its final file path.
-  model_mime_type = mimetypes.guess_type(model_path)[0] or default_mime_type
-
-  # Create payload.
-  payload = project_payload({
-    'name': name,
-    'ext': model_ext,
-    'file': (upload_file_key, open(model_path, 'rb'), model_mime_type)
-  })
-
-  # Create a multipart encoder.
-  encoder = MultipartEncoder(fields=payload)
-
-  # Create progress callback, specifying message to show when upload completes.
-  callback = create_callback(encoder, completion_log='\nUploading model...')
-
-  # Create a monitor for the encoder so we can track upload progress.
-  monitor = MultipartEncoderMonitor(encoder, callback)
-
-  # Set the request Content-Type to the multipart encoder's content type.
-  headers = {'Content-Type': monitor.content_type}
+  # Create model uploader.
+  uploader = new_file_uploader(model_path, completion_msg='\nUploading model...', name=name)
 
   try:
-    # Upload the model.
-    api.post('/model', headers=headers, mp_upload_monitor=monitor)
+    api.post('/model', headers={'Content-Type': uploader.content_type}, mp_upload_monitor=uploader)
   except KeyboardInterrupt:
     return
 
-  # TODO: Make this message more verbose, displaying the model version created.
   log('Successfully uploaded model.')
 
 
--- sweettea/utils/file_util.py
+++ sweettea/utils/file_util.py
@@ -2,11 +2,14 @@
 Utility methods related to files or their respective paths
 """
 import json
+import mimetypes
 import os
 import shutil
 import yaml
 import zipfile
-from sweettea.definitions import config_file_name, st_tmp_dir
+from clint.textui.progress import Bar as ProgressBar
+from requests_toolbelt.multipart.encoder import MultipartEncoder, MultipartEncoderMonitor
+from sweettea.definitions import config_file_name, default_mime_type
 from sweettea import log
 
 JSON_EXT = 'json'
@@ -145,16 +148,22 @@ def extract_in_place(archive_path):
 def zip_dir(src_dir, dest_zip_file_path):
   path = src_dir.rstrip('/') + '/'
 
+  # Ensure path exists.
   if not os.path.exists(path):
     log('No directory found at {}'.format(path))
     exit(1)
 
+  # Upsert parent directories of destination zip path.
   os.makedirs(os.path.dirname(dest_zip_file_path), exist_ok=True)
+
+  # Create new zip file.
   zf = zipfile.ZipFile(dest_zip_file_path, 'w', zipfile.ZIP_DEFLATED)
 
   try:
+    # Walk over all tree contents of path
     contents = os.walk(path)
 
+    # For each file and sub-dir inside source directory, write it into the zip file.
     for root, dirs, files in contents:
       for dir_name in dirs:
         dir_path = os.path.join(root, dir_name)
@@ -165,7 +174,118 @@ def zip_dir(src_dir, dest_zip_file_path):
         zf.write(file_path, file_path.replace(path, '', 1))
 
   except BaseException as e:
+    # Exit anytime an error occurs.
     log('Error occurred while zipping directory: {}'.format(e))
     exit(1)
   finally:
+    # Always close zipfile.
     zf.close()
+
+
+def new_file_uploader(path, name='file', completion_msg='Done!', **kwargs):
+  # Get file extension from path.
+  ext = file_ext(path)
+
+  # Append extension to name if extension is not empty.
+  if ext:
+    name = name + '.' + ext
+
+  # Guess mime type from file path.
+  mime_type = mimetypes.guess_type(path)[0] or default_mime_type
+
+  # Create payload for uploading with file info attached.
+  payload = kwargs
+  payload['file'] = (name, open(path, 'rb'), mime_type)
+  payload['ext'] = ext
+
+  # Create a multipart encoder.
+  encoder = MultipartEncoder(fields=payload)
+
+  # Create progress callback, specifying message to show when upload completes.
+  callback = file_upload_callback(encoder, completion_msg=completion_msg)
+
+  # Return monitor for the encoder so we can track upload progress.
+  return MultipartEncoderMonitor(encoder, callback)
+
+
+def file_upload_callback(encoder, completion_msg=None):
+  """
+  Create a progress callback function for a multi-part file upload
+
+  :param encoder:
+    Multipart file encoder
+    :type: requests_toolbelt.multipart.encoder.MultipartEncoder
+  :param str completion_msg:
+    Log to display when the file upload has completed
+  :return: Upload progress callback function
+  :rtype: function
+  """
+  # Create a progress bar with the given size, specifying which character to show for progress.
+  bar = ProgressBar(expected_size=encoder.len, filled_char='=')
+
+  # Function to be called as the upload progresses
+  def callback(monitor):
+    # If upload has completed...
+    if monitor.bytes_read == encoder.len:
+      # Hacking around this callback being called twice when completed.
+      if not hasattr(monitor, 'finished'):
+        setattr(monitor, 'finished', True)
+
+        # Show upload progress.
+        bar.show(monitor.bytes_read)
+
+        # Display completion log if provided.
+        if completion_msg:
+          log(completion_msg)
+    else:
+      # Show upload progress.
+      bar.show(monitor.bytes_read)
+
+  return callback
+
+
+class ProgressDownloadStream(object):
+  """
+  Progress bar buffer class that can monitor a file download by displaying
+  progress to the user while also writing to the desired file.
+
+  Basic usage:
+
+    download_stream = ProgressDownloadStream(stream=api_response_obj,
+                                             expected_size=total_file_bytes)
+
+    download_stream.stream_to_file(desired_file_path)
+
+  """
+
+  def __init__(self, stream=None, expected_size=None, chunk_size=512):
+    """
+    :param stream:
+      Streaming API response object returned by the 'requests' library
+      :type: requests.Response
+    :param int expected_size:
+      Total size of file being downloaded (in bytes)
+    :param int chunk_size:
+      Chunk size to use when iterating over streamed response content
+    """
+    self.stream = stream
+    self.prog_bar = ProgressBar(expected_size=expected_size, filled_char='=')
+    self.progress = 0
+    self.chunk_size = chunk_size
+
+  def stream_to_file(self, path):
+    """
+    Download file to given path and display progress to user
+
+    :param str path: Desired file path
+    """
+    # Using default state of files being overwritten for now
+    if os.path.exists(path):
+      os.remove(path)
+
+    # Stream downloaded contents to file and show progress
+    with open(path, 'wb') as f:
+      for chunk in self.stream.iter_content(chunk_size=self.chunk_size):
+        f.write(chunk)
+        self.progress += int(len(chunk))
+        self.prog_bar.show(self.progress)
